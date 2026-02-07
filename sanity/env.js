344b5f1d0c8eb9dc09e7b5d3aa1738aa3680499a
liabrenkier <liabrencontacto@gmail.com>
2026-02-07 16:34:17 -0300

    Agregar ocultar en sitio desde la terminal Sanity

--- sanity/env.js
+++ sanity/env.js
@@ -1,3 +1,3 @@
-export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2025-01-01";
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "";
+export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || process.env.SANITY_API_VERSION || "2025-01-01";
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || process.env.SANITY_DATASET || "production";
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || process.env.SANITY_PROJECT_ID || "";
